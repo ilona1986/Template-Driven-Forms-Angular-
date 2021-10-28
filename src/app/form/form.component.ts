e2f126218f4809835c8525be00171a71c6fa15cb
Ilona <ilona_b9ka@mail.ru>
2021-10-28 16:20:20 +0300

    template driven forms

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {User} from "../shared/user";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, AfterViewInit {
 
   roles: string[] = ['Гость', 'Модератор', 'Администратор']
   model: User = new User(1, null, null, null)
+
+  formErrors: any = {
+    name: '',
+    age: ''
+  }
+
+  validationMessages: any = {
+    name: {
+      required: 'Имя обязкательно.',
+      minlength: 'Имя должно содержать минимум 2 символа'
+    },
+    age: {
+      required: 'Возраст обязателен.'
+    }
+  }
+
+  @ViewChild('userForm') userForm: NgForm | null = null
 
   constructor() {
   }
@@ -17,11 +35,30 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.userForm?.valueChanges?.subscribe(data => this.onValueChanged())
+  }
+
   onSubmit(): void {
     console.log('Форма отправлена');
   }
 
-  errorsLog(d: any): void {
+  private onValueChanged(): void {
+    const form: any = this.userForm?.form
 
+    Object.keys(this.formErrors).forEach(field => {
+      this.formErrors[field] = '';
+
+      const control = form?.get(field);
+
+      if (control && control.dirty && control.invalid) {
+        const message = this.validationMessages[field];
+
+        Object.keys(control.errors).forEach(key => {
+
+          this.formErrors[field] += message[key] + ' '
+        })
+      }
+    })
   }
 }
